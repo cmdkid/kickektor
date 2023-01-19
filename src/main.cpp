6c650b70ccca145622ac35f5cbc0be5eb43598c9
dsdr <qmmsdd@gmail.com>
2023-01-19 20:56:26 +0300

    float measure script

--- src/main.cpp
+++ src/main.cpp
@@ -1,206 +1,84 @@
+/* FFT_TEST4
+ Ray Burnette 20130810 function clean-up & 1284 port (328 verified)
+ Modified by varind in 2013: this code is public domain, enjoy!
+ http://www.variableindustries.com/audio-spectrum-analyzer/
+ 328P = Binary sketch size: 5,708 bytes (of a 32,256 byte maximum)
+ */
 #include <Arduino.h>
+#include "fix_fft.h"  // fix_fft.ccp & fix_fft.h in same directory as sketch
 
-#define FHT_N 64         // ширина спектра х2
-#define LOG_OUT 1
-#include <FHT.h>         // преобразование Хартли
+#define LCHAN 3
 
-#define SOUND_R_FREQ A3  // analog audio in (through capasitor)
+const int Yres = 8;
+const int gain = 3;
+float peaks[64];
+char im[64], data[64];
+char data_avgs[64];
+int debugLoop;
 
-// relay triggers setup
-#define TRIG_COUNT 4 // ON VAR CHANGE: update func setTrigerStatus,initTriggers and define TRIG_N
-bool trigStatus[TRIG_COUNT];  // current triggers status
-// digital pins to relay trigger
-#define TRIG_1 2  
-#define TRIG_2 3
-#define TRIG_3 4
-#define TRIG_4 5
-// trigger fht_log_out min,max range
-//int trig_range[TRIG_COUNT*2];
-// trigger frequency range
-//uint16_t trig_freq[TRIG_COUNT*2];
-// tmp cycle trigger status
-bool tmpTrigStatus[TRIG_COUNT];
-int trigFoundCount[TRIG_COUNT];
-//DELETE ME
-//uint16_t max_freq_val = 0;
-//int max_freq_id = 0;
 
-void setTrigerStatus(int trig_id, bool value) {
-  int trig_pin = 0;
-  switch (trig_id) {
-    case 0:
-      trig_pin = TRIG_1;
-      break;
-    case 1:
-      trig_pin = TRIG_2;
-      break;
-    case 2:
-      trig_pin = TRIG_3;
-      break;
-    case 3:
-      trig_pin = TRIG_4;
-      break;
-  }
-  if (trig_pin > 0) {
-    // we use triggers, who works with zero volt trigger, so "ON" is low voltage, and "OFF" is high voltage
-    if (false == value) {
-      digitalWrite(trig_pin, LOW); //HIGH
-    } else {
-      digitalWrite(trig_pin, HIGH);
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+void Two16_LCD(){
+  for (int x = 1; x < 16; x++) {  // init 0 to show lowest band overloaded with hum
+    if (data_avgs[x] > peaks[x]) peaks[x] = data_avgs[x];
+
+    if (peaks[x] == 0) {
+      Serial.print(" .   ");  // less LCD artifacts than " "
     }
-    Serial.print((String)"D" + String(trig_pin) + ">" + String(value) + "|");
-  }
-}
-
-// set trigger vars
-void initTriggers() {
-  pinMode(TRIG_1, OUTPUT);
-  pinMode(TRIG_2, OUTPUT);
-  pinMode(TRIG_3, OUTPUT);
-  pinMode(TRIG_4, OUTPUT);
-
-  for (int i = 0; i < TRIG_COUNT; i ++) {
-    trigStatus[i] = false;
-    tmpTrigStatus[i] = false;
-    setTrigerStatus(i, false);
-    trigFoundCount[i] = 0;
-  }
-}
-
-void relay_blink(int relay_id, int delay_time) {
-    setTrigerStatus(relay_id, true);
-    delay(delay_time);
-    setTrigerStatus(relay_id, false);
-}
-
-void trigger(int relay_id, int delay_time, bool trigger_found = true) {
-  if (trigger_found == false) {
-    if (trigFoundCount[relay_id] != 0) {
-      relay_blink(relay_id, delay_time);
+    else {
+      Serial.print(peaks[x]);
+      Serial.print(" ");
     }
-    trigFoundCount[relay_id] = 0;
-  } else {
-    trigFoundCount[relay_id] += 1;
   }
-  /*
-  if (trigger_found == true) {
-    trigFoundCount[relay_id] += 1;
-    if (trigFoundCount[relay_id] > 1) {
-      trigFoundCount[relay_id] = 0;
-      relay_blink(relay_id, delay_time);
-    }
-  } else {
-    trigFoundCount[relay_id] = 0;
-  }
-  */
-  
-  
+  Serial.println("");
 }
 
-// init vals
+void decay(int decayrate){
+  int DecayTest = 1;
+  // reduce the values of the last peaks by 1 
+  if (DecayTest == decayrate){
+    for (int x = 0; x < 32; x++) {
+      peaks[x] = peaks[x] - 1;  // subtract 1 from each column peaks
+      DecayTest = 0;
+    }
+  }
+
+  DecayTest++;
+}
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("Init"));
-  initTriggers();
-
-  for (int i = 0; i < TRIG_COUNT; i ++) {
-    relay_blink(i, 500);
-    delay(100);
-  }
-  delay(1000);
-
-  // debug init vals
-  /*
-  trig_range[0] = 2;
-  trig_range[1] = 31;
-  trig_freq[0] = 40;
-  trig_freq[1] = 20000;
-  */
-  Serial.println(F("Start"));
+  Serial.begin(9600); // hardware serial
+  Serial.print("Debug ON");
+  Serial.println("");
 }
 
-// get range of input sound values
-void analyzeAudio() {
-  for (int i = 0 ; i < FHT_N ; i++) {
-    int sample = analogRead(SOUND_R_FREQ);
-    fht_input[i] = sample; // put real data into bins
-  }
-  fht_window();  // window the data for better frequency response
-  fht_reorder(); // reorder the data before doing the fht
-  fht_run();     // process the data in the fht
-  fht_mag_log(); // take the output of the fht
-}
-
-void triggersLoop() {
-  // get sound data
-  analyzeAudio();
-  //Serial.print("fht_log_out:");
-  //max_freq_val = 0;
-  //max_freq_id = 0;
-
-  if (fht_log_out[2] >= 88){
-    //relay_blink(0, 50);
-    trigger(0, 50);
-  } else {
-    trigger(0, 50, false);
-  }
-
-  //if (fht_log_out[7] >= 40){
-  //  relay_blink(1, 10);
-  //}
-  
-  /*
-  if (fht_log_out[2] >= 63 && fht_log_out[2] < 90){
-    relay_blink(2, 10);
-  }
-  */
-  
-  for(int i = 2; i < 32; i++) {
-
-    if (fht_log_out[i] >= 20) {
-      Serial.print(String(fht_log_out[i]));
-    } else {
-      Serial.print("00");
-    }
-    Serial.print(" ");
-    //if (fht_log_out[i] > max_freq_val) {
-    //  max_freq_val = fht_log_out[i];
-    //  max_freq_id = i;
-    //}
-  }
-
-  
-  //Serial.print("max_freq_id:" + String(max_freq_id) + " freq_val=" + String(max_freq_val));
-  /*
-  // fht_log_out 0 and 1 are noisy, ignore it
-  for (int i = 2; i < 32; i++) {
-    Serial.print(String(fht_log_out[i]) + ",");
-    // for every trigger
-    for (int j = 0; j < TRIG_COUNT; j++) {
-        // check range min/max
-        if (i >= trig_range[j*2] && i <= trig_range[j*2+1]) {
-            // check freq range
-            if (fht_log_out[i] >= trig_freq[j*2] && fht_log_out[i] <= trig_freq[j*2+1]) {
-                tmpTrigStatus[j] = true;
-            }
-            else {
-                tmpTrigStatus[j] = false;
-            }
-        }
-    }
-  }
-  Serial.print(F(">>"));
-  //update relay status
-  for (int i = 0; i < TRIG_COUNT; i++) {
-    setTrigerStatus(i, tmpTrigStatus[i]);
-  }
-  */
-
-  Serial.println(F("."));
-  //delay(10);
-}
-
-// main loop
 void loop() {
-  triggersLoop();
+  for (int i = 0; i < 64; i++) {
+    data[i]  = ((analogRead(LCHAN) / 4 ) - 128);  // chose how to interpret the data from analog in  
+    im[i]  = 0;   // imaginary component  
+  }
+
+  fix_fft(data, im, 6, 0);   // Send Left channel normalized analog values through fft
+
+  // At this stage, we have two arrays of [0-31] frequency bins deep [32-63] duplicate
+
+  // calculate the absolute values of bins in the array - only want positive values
+  for (int i = 0; i < 16; i++) {
+    data[i] = sqrt(data[i]  *  data[i] +  im[i] *  im[i]);
+
+    // COPY the Right low-band (0-15) into the Left high-band (16-31) for display ease
+    data_avgs[i] = data[i];
+
+    // Remap values to physical display constraints... that is, 8 display custom character indexes + "_"
+    //data_avgs[i] = constrain(data_avgs[i], 0, 9 - gain);     //data samples * range (0-9) = 9
+    //data_avgs[i] = map(data_avgs[i], 0, 9 - gain, 0, Yres);  // remap averaged values
+  }
+
+  Two16_LCD();
+  decay(1);
 }
